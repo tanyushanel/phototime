1f5da6922b67252368fe77dd30cc9e230ef0e12f
tanyushanel <tanyushanel@users.noreply.github.com>
2021-02-03 20:35:23 +0300

    switch, navbar css

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -1,3 +1,4 @@
+import { ThisReceiver } from '@angular/compiler';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,9 +7,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./main.component.scss'],
 })
 export class MainComponent implements OnInit {
+  start: number = 1;
+  count: number = 5;
+  stepValue: number = 1;
+
+  images: string[] = [
+    '../../assets/Livello-slide-1.png',
+    '../../assets/Livello-slide-2.png',
+    '../../assets/Livello-bg1.png',
+    '../../assets/Livello-bg2.png',
+  ];
+
+  img: string;
+
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.img = this.images[0];
+  }
 
-  switchCalendar(event) {}
+  goToImg(dir: number): void {
+    let arr = this.images;
+
+    this.img = this.images.splice(dir, 1).join(''); // deleted
+    dir ? arr.splice(0, 0, this.img) : arr.splice(arr.length, 0, this.img);
+  }
+
+  go(step): void {
+    this.stepValue += step;
+  }
 }
